a95376f2e8ac9a8e9346e98523d4cb3cbb2f247f
Ioann Volkov <volkov.ioann@gmail.com>
2025-04-06 12:40:48 +0300

    Measure a lot of stats in test client:
    Delay from first part sent till first part received: 62.50 ms
    Sending all parts took: 56.96 ms
    Receiving all parts took: 8948.15 ms
    Original audio duration: 25.250 s
    Received audio duration: 25.350 s
    Receiving was 2.83 times faster than realtime

--- infer-websocket-test-client.py
+++ infer-websocket-test-client.py
@@ -3,6 +3,7 @@
 import os
 import re
 import random
+import time
 import asyncio
 import websockets
 import numpy as np
@@ -53,32 +54,55 @@ async def main():
     audio_files_parts = read_audio_files_parts()
     chosen_audio_key = random.choice(list(audio_files_parts.keys()))
 
+    first_part_sent_ts = None
+    first_part_received_ts = None
+    end_message_received_ts = None
+    received_total = 0
+
     async with websockets.connect(URL, additional_headers={'Authorization': BEARER_PREFIX + AUTH_TOKEN}) as websocket:
         async def receiver():
+            nonlocal first_part_received_ts, end_message_received_ts, received_total
             buffer = b''
             while True:
                 try:
                     data = await asyncio.wait_for(websocket.recv(), timeout=1)
                     if isinstance(data, bytes):
+                        if first_part_received_ts is None:
+                            first_part_received_ts = time.time()
                         print(f'Received data of size {len(data)}')
                         buffer += data
                     elif isinstance(data, str):
                         if data == 'end_message':
+                            end_message_received_ts = time.time()
                             print('Received end_message')
                             break
                 except asyncio.TimeoutError:
                     continue  # periodically checking if we need to stop
             saveToWav(filename=f'{TEST_DATA_DIR}/OUTPUT.wav', audioData=buffer)
+            received_total = len(buffer)
             print('Receiver stopped')
 
         receive_task = asyncio.create_task(receiver())
+        sent_total = 0
         for i, audio_part in enumerate(audio_files_parts[chosen_audio_key], start=1):
-            print(f'Sending "{chosen_audio_key}", part {i}')
+            print(f'Sending "{chosen_audio_key}", part {i}, size {len(audio_part)}')
             await websocket.send(audio_part)
-            audio_part_duration = len(audio_part) / 24000 / 2
-            await asyncio.sleep(audio_part_duration / 4)  # simulate stream which is 4 times faster than realtime
+            sent_total += len(audio_part)
+            if first_part_sent_ts is None:
+                first_part_sent_ts = time.time()
+            #audio_part_duration = len(audio_part) / 24000 / 2
+            #await asyncio.sleep(audio_part_duration / 4)  # simulate stream which is 4 times faster than realtime
         await websocket.send('end_message')
+        end_message_sent_ts = time.time()
         await receive_task
+        print(f'Delay from first part sent till first part received: {(first_part_received_ts - first_part_sent_ts) * 1000:.2f} ms')
+        print(f'Sending all parts took: {(end_message_sent_ts - first_part_sent_ts) * 1000:.2f} ms')
+        receiving_elapsed = end_message_received_ts - first_part_received_ts
+        print(f'Receiving all parts took: {receiving_elapsed * 1000:.2f} ms')
+        print(f'Original audio duration: {sent_total / 24000 / 2:.3f} s')
+        received_audio_duration = received_total / 24000 / 2
+        print(f'Received audio duration: {received_audio_duration:.3f} s')
+        print(f'Receiving was {received_audio_duration / receiving_elapsed:.2f} times faster than realtime')
 
 if __name__ == '__main__':
     asyncio.run(main())
