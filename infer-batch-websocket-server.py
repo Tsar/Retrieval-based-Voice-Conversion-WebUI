2327846ce764eafe586490e1a4e2ce6438a97caf
Ioann Volkov <volkov.ioann@gmail.com>
2025-04-12 23:14:49 +0300

    infer-batch-websocket-server: Batch inference executed, handling result & post processing left

--- infer-batch-websocket-server.py
+++ infer-batch-websocket-server.py
@@ -15,6 +15,17 @@ import asyncio
 import websockets
 import numpy as np
 
+import fairseq.checkpoint_utils
+from fairseq.models.hubert import HubertModel
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchaudio.transforms as tat
+from torchfcpe import spawn_bundled_infer_model
+from torchfcpe.models_infer import InferCFNaiveMelPE
+
+from infer.lib.jit.get_synthesizer import get_synthesizer
+
 logger = logging.getLogger('infer-batch-websocket-server')
 
 BEARER_PREFIX = 'Bearer '
@@ -37,27 +48,98 @@ TARGET_VOICES_PITCH = {
     'voicevox_speaker_43': 8,
 }
 
+SUPPORTED_TARGET_VOICES = list(TARGET_VOICES_PITCH.keys())
+
+MODEL_PTH_PATH = 'assets/weights/voicevox_speaker_43.pth'
+
+GPU = 'cuda:0'
+IS_HALF = True
+MAX_INFERENCE_BATCH_SIZE = 10
+
+SAMPLE_RATE = 24000  # for both input and output
 BLOCK_DURATION_MS = 150
-BLOCK_SIZE = 24000 * 2 * BLOCK_DURATION_MS // 1000  # PCM16, 24 KHz, mono audio
+CROSSFADE_DURATION_MS = 80
+EXTRA_TIME_MS = 2000  # TODO: Reduce
+
+BLOCK_FRAME = SAMPLE_RATE * BLOCK_DURATION_MS // 1000
+BLOCK_FRAME_16K = 16000 * BLOCK_FRAME // SAMPLE_RATE
+F0_EXTRACTOR_FRAME = BLOCK_FRAME_16K + 800
+BLOCK_SIZE_BYTES = BLOCK_FRAME * 2  # PCM16
+CROSSFADE_FRAME = SAMPLE_RATE * CROSSFADE_DURATION_MS // 1000
+ZC = SAMPLE_RATE // 100
+SOLA_BUFFER_FRAME = min(CROSSFADE_FRAME, 4 * ZC)
+SOLA_SEARCH_FRAME = ZC
+EXTRA_FRAME = SAMPLE_RATE * EXTRA_TIME_MS // 1000
+SKIP_HEAD = EXTRA_FRAME // ZC
+RETURN_LENGTH = (BLOCK_FRAME + SOLA_BUFFER_FRAME + SOLA_SEARCH_FRAME) // ZC
+PITCH_SHIFT = BLOCK_FRAME_16K // 160
+
+INPUT_WAV_LEN = EXTRA_FRAME + CROSSFADE_FRAME + SOLA_SEARCH_FRAME + BLOCK_FRAME
+INPUT_WAV_RES_LEN = 160 * INPUT_WAV_LEN // ZC
+P_LEN = INPUT_WAV_RES_LEN // 160
+
+F0_MIN = 50
+F0_MAX = 1100
+F0_MEL_MIN = 1127 * np.log(1 + F0_MIN / 700)
+F0_MEL_MAX = 1127 * np.log(1 + F0_MAX / 700)
+
+RESAMPLER_TO_16K = tat.Resample(
+    orig_freq=SAMPLE_RATE,
+    new_freq=16000,
+    dtype=torch.float32,
+).to(GPU)
+
+class ClientContext:
+    def __init__(self, pitch: int, formant_shift: float = 0.0):
+        self.pitch = pitch
+        self.formant_shift = formant_shift
+        self.input_wav: torch.Tensor = torch.zeros(
+            INPUT_WAV_LEN,
+            device=GPU,
+            dtype=torch.float32,
+        )
+        self.input_wav_res: torch.Tensor = torch.zeros(
+            INPUT_WAV_RES_LEN,
+            device=GPU,
+            dtype=torch.float32,
+        )
+        self.cache_pitch = torch.zeros(1024, device=GPU, dtype=torch.long)
+        self.cache_pitchf = torch.zeros(1024, device=GPU, dtype=torch.float32)
+
+    # TODO: Fill with zeros again on message end?
+
+    def prepare_input_buffers(self, block_i16: bytes):
+        block_f32 = np.frombuffer(block_i16, dtype=np.int16).astype(np.float32) / 32768.0
+        assert block_f32.shape[0] == BLOCK_FRAME
+        self.input_wav[:-BLOCK_FRAME] = self.input_wav[BLOCK_FRAME:].clone()
+        self.input_wav[-BLOCK_FRAME:] = torch.from_numpy(block_f32).to(GPU)
+        self.input_wav_res[:-BLOCK_FRAME_16K] = self.input_wav_res[BLOCK_FRAME_16K:].clone()
+        self.input_wav_res[-BLOCK_FRAME_16K - 160:] = RESAMPLER_TO_16K(self.input_wav[-BLOCK_FRAME - 2 * ZC:])[160:]
+
+    def update_pitch_caches(self, pitch: torch.Tensor, pitchf: torch.Tensor):
+        self.cache_pitch[:-PITCH_SHIFT] = self.cache_pitch[PITCH_SHIFT:].clone()
+        self.cache_pitchf[:-PITCH_SHIFT] = self.cache_pitchf[PITCH_SHIFT:].clone()
+        self.cache_pitch[4 - pitch.shape[0]:] = pitch[3:-1]
+        self.cache_pitchf[4 - pitch.shape[0]:] = pitchf[3:-1]
 
 class Task:
     def __init__(
         self,
         priority: int,
         sequence: int,
-        target_voice: str,
-        transpose_by: int,
-        block: bytes,
-        is_last_block: bool,
-        websocket,
+        feats: torch.Tensor,
+        cache_pitch: torch.Tensor,
+        cache_pitchf: torch.Tensor,
+        return_length2: int,
+        future: asyncio.Future,
     ):
         self.priority = priority
         self.sequence = sequence
-        self.target_voice = target_voice
-        self.transpose_by = transpose_by
-        self.block = block
-        self.is_last_block = is_last_block
-        self.websocket = websocket
+        self.feats = feats
+        self.cache_pitch = cache_pitch
+        self.cache_pitchf = cache_pitchf
+        self.return_length2 = return_length2
+        self.future = future
 
     def __lt__(self, other) -> bool:
         if self.priority != other.priority:
@@ -65,25 +147,154 @@ class Task:
         return self.sequence < other.sequence
 
 global_sequence = itertools.count()
+executor = ThreadPoolExecutor(max_workers=cpu_count() * 2)  # TODO: Replace with ProcessPoolExecutor?
 
-preprocess_queue = asyncio.PriorityQueue()
+# Each target voice has its own priority queue
+inference_queues = {}
+for voice in SUPPORTED_TARGET_VOICES:
+    inference_queues[voice] = asyncio.PriorityQueue()
 
-executor = ThreadPoolExecutor(max_workers=cpu_count())  # TODO: Replace with ProcessPoolExecutor?
+hubert_model: Optional[HubertModel] = None
+fcpe_model: Optional[InferCFNaiveMelPE] = None
+net_g_model: Optional[nn.Module] = None  # TODO: Support multiple net_g models
 
-async def preprocess_queue_dispatch_loop():
-    loop = asyncio.get_running_loop()
+def load_hubert_model():
+    global hubert_model
+    load_start_time = time.perf_counter()
+
+    models, _, _ = fairseq.checkpoint_utils.load_model_ensemble_and_task(
+        ["assets/hubert/hubert_base.pt"],
+        suffix="",
+    )
+    hubert_model = models[0]
+    hubert_model = hubert_model.to(GPU)
+    if IS_HALF:
+        hubert_model = hubert_model.half()
+    else:
+        hubert_model = hubert_model.float()
+    hubert_model.eval()
+
+    load_done_time = time.perf_counter()
+    logger.info(f'Loaded Hubert model in {(load_done_time - load_start_time) * 1000:.1f} ms')
+
+def load_fcpe_model():
+    global fcpe_model
+    load_start_time = time.perf_counter()
+    fcpe_model = spawn_bundled_infer_model(device=GPU)
+    load_done_time = time.perf_counter()
+    logger.info(f'Loaded fcpe model in {(load_done_time - load_start_time) * 1000:.1f} ms')
+
+def load_net_g_model(pth_path):
+    global net_g_model
+    load_start_time = time.perf_counter()
+
+    net_g_model, cpt = get_synthesizer(pth_path=pth_path, device=GPU)
+    tgt_sr = cpt["config"][-1]  # TODO: Use for resampler 2
+    cpt["config"][-3] = cpt["weight"]["emb_g.weight"].shape[0]
+    assert cpt.get("f0", 1) == 1
+    assert cpt.get("version", "v1") == "v2"
+    if IS_HALF:
+        net_g_model = net_g_model.half()
+    else:
+        net_g_model = net_g_model.float()
+
+    load_done_time = time.perf_counter()
+    logger.info(f'Loaded net_g model in {(load_done_time - load_start_time) * 1000:.1f} ms')
+
+def extract_features(input_wav: torch.Tensor, version="v2"):
+    with torch.no_grad():
+        if IS_HALF:
+            feats = input_wav.half().view(1, -1)
+        else:
+            feats = input_wav.float().view(1, -1)
+        padding_mask = torch.BoolTensor(feats.shape).to(GPU).fill_(False)
+        inputs = {
+            "source": feats,
+            "padding_mask": padding_mask,
+            "output_layer": 9 if version == "v1" else 12,
+        }
+        logits = hubert_model.extract_features(**inputs)
+        feats = (
+            hubert_model.final_proj(logits[0]) if version == "v1" else logits[0]
+        )
+        feats = torch.cat((feats, feats[:, -1:, :]), 1)
+    return feats
+
+def get_f0_fcpe(input_wav: torch.Tensor, f0_up_key: float):
+    f0 = fcpe_model.infer(
+        input_wav.to(GPU).unsqueeze(0).float(),
+        sr=16000,
+        decoder_mode="local_argmax",
+        threshold=0.006,
+    )
+    f0 *= pow(2, f0_up_key / 12)
+    f0 = f0.float().to(GPU).squeeze()
+    f0_mel = 1127 * torch.log(1 + f0 / 700)
+    f0_mel[f0_mel > 0] = (f0_mel[f0_mel > 0] - F0_MEL_MIN) * 254 / (F0_MEL_MAX - F0_MEL_MIN) + 1
+    f0_mel[f0_mel <= 1] = 1
+    f0_mel[f0_mel > 255] = 255
+    f0_coarse = torch.round(f0_mel).long()
+    return f0_coarse, f0
+
+# Should be executed sequentially for each context, can be executed in parallel for different contexts
+def prepare_task_for_inference(context: ClientContext, block_i16: bytes, priority: int, future: asyncio.Future) -> Task:
+    context.prepare_input_buffers(block_i16=block_i16)
+    input_wav = context.input_wav_res
+
+    feats = extract_features(input_wav=input_wav)  # TODO: Use batched inference here if Hubert can
+
+    pitch, pitchf = get_f0_fcpe(  # TODO: Use batched inference here if fcpe can
+        input_wav=input_wav[-F0_EXTRACTOR_FRAME:],
+        f0_up_key=context.pitch - context.formant_shift,
+    )
+    factor = pow(2, context.formant_shift / 12)
+    return_length2 = int(np.ceil(RETURN_LENGTH * factor))
+    context.update_pitch_caches(pitch, pitchf)
+    cache_pitch = context.cache_pitch[None, -P_LEN:]
+    cache_pitchf = context.cache_pitchf[None, -P_LEN:] * return_length2 / RETURN_LENGTH
+
+    feats = F.interpolate(feats.permute(0, 2, 1), scale_factor=2).permute(0, 2, 1)
+    feats = feats[:, :P_LEN, :]
+    return Task(
+        priority=priority,
+        sequence=next(global_sequence),
+        feats=feats,
+        cache_pitch=cache_pitch,
+        cache_pitchf=cache_pitchf,
+        return_length2=return_length2,
+        future=future,
+    )
+
+async def net_g_inference_worker(net_g: nn.Module, tasks_queue: asyncio.PriorityQueue):
     while True:
-        task: Task = await preprocess_queue.get()
-        try:
-            loop.run_in_executor(executor, preprocess, task)
-        finally:
-            preprocess_queue.task_done()
+        tasks_batch = [await tasks_queue.get()]
+        for _ in range(MAX_INFERENCE_BATCH_SIZE - 1):
+            try:
+                tasks_batch.append(tasks_queue.get_nowait())
+            except asyncio.QueueEmpty:
+                break
+        B = len(tasks_batch)
+        feats = torch.cat([task.feats for task in tasks_batch], dim=0)
+        p_len = torch.full((B,), P_LEN, dtype=torch.long, device=GPU)
+        cache_pitch = torch.cat([task.cache_pitch for task in tasks_batch], dim=0)
+        cache_pitchf = torch.cat([task.cache_pitchf for task in tasks_batch], dim=0)
+        sid = torch.zeros(B, dtype=torch.long, device=GPU)
+        skip_head = torch.full((B,), SKIP_HEAD, dtype=torch.long)  # TODO: device=GPU??
+        return_length = torch.full((B,), RETURN_LENGTH, dtype=torch.long)  # TODO: device=GPU??
+        return_length2 = torch.tensor([task.return_length2 for task in tasks_batch], dtype=torch.long)  # TODO: device=GPU??
 
-def preprocess(task: Task):
-    assert len(task.block) == BLOCK_SIZE
-    block_f32 = np.frombuffer(task.block, dtype=np.int16).astype(np.float32) / 32768.0
-    # TODO
-
+        logger.info(f'Running net_g inference for batch of size {B}')
+        result = net_g.infer(
+            feats,
+            p_len,
+            cache_pitch,
+            cache_pitchf,
+            sid,
+            skip_head,
+            return_length,
+            return_length2,
+        )
+        # TODO
 
 def error_message(message, log_prefix='', details_to_log=None):
     if details_to_log:
@@ -151,48 +362,72 @@ async def handler(websocket):
     logger.info(f'{log_prefix}Starting voice conversion to {target_voice} transposed by {transpose_by}')
 
     buffer = b''
-    block_num = 0
-    msg_start_ts_ms: Optional[int] = None
+    preprocess_blocks_queue = asyncio.Queue()
+    postprocess_tasks_queue = asyncio.Queue()
+    stop_event = asyncio.Event()
 
     try:
+        async def preprocessing_loop():
+            msg_start_ts_ms: Optional[int] = None
+            block_num = 0
+            context = ClientContext(pitch=transpose_by)
+            while not stop_event.is_set():
+                try:
+                    block, is_last = await asyncio.wait_for(preprocess_blocks_queue.get(), timeout=1)
+                    if msg_start_ts_ms is None:
+                        msg_start_ts_ms = int(time.time() * 1000)
+
+                    assert isinstance(block, bytes)
+                    assert isinstance(is_last, bool)
+                    assert len(block) == BLOCK_SIZE_BYTES
+
+                    loop = asyncio.get_running_loop()
+                    task = await loop.run_in_executor(
+                        executor,
+                        prepare_task_for_inference,
+                        context,
+                        block,
+                        msg_start_ts_ms + BLOCK_DURATION_MS * block_num,
+                        loop.create_future(),
+                    )
+                    postprocess_tasks_queue.put_nowait(task)
+                    inference_queues[target_voice].put_nowait(task)
+                    block_num += 1
+
+                    if is_last:
+                        msg_start_ts_ms = None
+                        block_num = 0
+                except asyncio.TimeoutError:
+                    continue  # periodically checking if we need to stop
+            logger.info(f'{log_prefix}Preprocessing loop stopped gracefully')
+
+        async def postprocessing_loop():
+            while not stop_event.is_set():
+                try:
+                    task: Task = await asyncio.wait_for(postprocess_tasks_queue.get(), timeout=1)
+                    result = await task.future
+
+                    # TODO
+
+                except asyncio.TimeoutError:
+                    continue  # periodically checking if we need to stop
+            logger.info(f'{log_prefix}Postprocessing loop stopped gracefully')
+
+        asyncio.create_task(preprocessing_loop())
+        asyncio.create_task(postprocessing_loop())
+
         while True:
             data = await websocket.recv()
-            if msg_start_ts_ms is None:
-                msg_start_ts_ms = int(time.time() * 1000)
-
             if isinstance(data, bytes):
                 buffer += data
-                while len(buffer) >= BLOCK_SIZE:
-                    preprocess_queue.put_nowait(
-                        Task(
-                            priority=msg_start_ts_ms + BLOCK_DURATION_MS * block_num,
-                            sequence=next(global_sequence),
-                            target_voice=target_voice,
-                            transpose_by=transpose_by,
-                            block=buffer[:BLOCK_SIZE],
-                            is_last_block=False,
-                            websocket=websocket,
-                        )
-                    )
-                    buffer = buffer[BLOCK_SIZE:]
-                    block_num += 1
+                while len(buffer) >= BLOCK_SIZE_BYTES:
+                    preprocess_blocks_queue.put_nowait((buffer[:BLOCK_SIZE_BYTES], False))
+                    buffer = buffer[BLOCK_SIZE_BYTES:]
             elif isinstance(data, str):
                 if data == 'end_message':
-                    assert len(buffer) < BLOCK_SIZE
-                    preprocess_queue.put_nowait(
-                        Task(
-                            priority=msg_start_ts_ms + BLOCK_DURATION_MS * block_num,
-                            sequence=next(global_sequence),
-                            target_voice=target_voice,
-                            transpose_by=transpose_by,
-                            block=buffer.ljust(BLOCK_SIZE, b'\x00'),
-                            is_last_block=True,
-                            websocket=websocket,
-                        )
-                    )
+                    assert len(buffer) < BLOCK_SIZE_BYTES
+                    preprocess_blocks_queue.put_nowait((buffer.ljust(BLOCK_SIZE_BYTES, b'\x00'), True))
                     buffer = b''
-                    msg_start_ts_ms = None
-                    block_num = 0
                 else:
                     logger.error(f'{log_prefix}Unrecognized text received: "{data}"')
             else:
@@ -202,6 +437,8 @@ async def handler(websocket):
         logger.info(f'{log_prefix}Disconnected OK: {ex}')
     except websockets.exceptions.ConnectionClosedError as ex:
         logger.error(f'{log_prefix}Disconnected with error: {ex}')
+    finally:
+        stop_event.set()
 
 async def main():
     ssl_context = None
@@ -216,7 +453,12 @@ async def main():
             logger.warning('SSL certificates NOT FOUND, unencrypted serving prohibited')
             return
 
-    asyncio.create_task(preprocess_queue_dispatch_loop())
+    load_hubert_model()
+    load_fcpe_model()
+    load_net_g_model(pth_path=MODEL_PTH_PATH)  # TODO: Load multiple models
+
+    # TODO: Create task for each voice
+    asyncio.create_task(net_g_inference_worker(net_g_model, inference_queues['voicevox_speaker_43']))
 
     try:
         async with websockets.serve(handler, host='', port=PORT, ssl=ssl_context) as server:
