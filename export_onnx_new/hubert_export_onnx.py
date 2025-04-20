eeb43b89df369fe46eb66bcaa7e8f55a55840a33
Ioann Volkov <ioann.volkov@wolt.com>
2025-04-20 15:12:48 +0300

    New tools to export required models to ONNX. Only works for fcpe at the moment

---
+++ export_onnx_new/hubert_export_onnx.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import time
+from typing import Optional
+
+import fairseq.checkpoint_utils
+from fairseq.models.hubert import HubertModel
+import torch
+import torch.nn as nn
+
+DATA_DIR = '../load_tests/data'
+
+GPU = 'cuda:0'
+IS_HALF = True
+
+hubert_model: Optional[HubertModel] = None
+
+def load_hubert_model():
+    global hubert_model
+    load_start_time = time.perf_counter()
+
+    models, _, _ = fairseq.checkpoint_utils.load_model_ensemble_and_task(
+        ["../assets/hubert/hubert_base.pt"],
+        suffix="",
+    )
+    hubert_model = models[0]
+    hubert_model = hubert_model.to(GPU)
+    if IS_HALF:
+        hubert_model = hubert_model.half()
+    else:
+        hubert_model = hubert_model.float()
+    hubert_model.eval()
+
+    load_done_time = time.perf_counter()
+    print(f'Loaded Hubert model in {(load_done_time - load_start_time) * 1000:.1f} ms')
+
+C_input_wav_batch = torch.load(f'{DATA_DIR}/input_wav_batch.pt').to(GPU)
+assert C_input_wav_batch.shape == torch.Size([10, 35840])
+C_padding_mask = torch.BoolTensor(C_input_wav_batch.shape).to(GPU).fill_(False)
+assert C_padding_mask.shape == torch.Size([10, 35840])
+
+class HubertExtractFeaturesWrapper(nn.Module):
+    def __init__(self, orig_hubert_model: HubertModel):
+        super().__init__()
+        self.model = orig_hubert_model
+
+    def forward(self, source, padding_mask) -> torch.Tensor:  # only pass Tensors here
+        return self.model.extract_features(
+            source=source,
+            padding_mask=padding_mask,
+            output_layer=12,
+        )[0]
+
+def export_to_onnx():
+    model = HubertExtractFeaturesWrapper(orig_hubert_model=hubert_model)
+    model.eval()
+
+    torch.onnx.export(
+        model,
+        (
+            C_input_wav_batch,  # source
+            C_padding_mask,     # padding_mask
+        ),
+        'hubert_export_features.onnx',
+        input_names=['input_wav', 'padding_mask'],
+        output_names=['features'],
+        dynamic_axes={
+            'input_wav': {0: 'batch_size', 1: 'audio_len'},
+            'padding_mask': {0: 'batch_size', 1: 'audio_len'},
+            'features': {0: 'batch_size', 1: 'sequence_len'},
+        },
+        opset_version=17,
+        export_params=True,
+        do_constant_folding=True,
+        dynamo=True,
+    )
+
+if __name__ == '__main__':
+    load_hubert_model()
+    export_to_onnx()
---
+++ export_onnx_new/net_g_export_onnx.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import time
+from typing import Optional
+
+import torch
+import torch.nn as nn
+
+root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, root)
+from infer.lib.jit.get_synthesizer import get_synthesizer
+
+DATA_DIR = '../load_tests/data'
+
+GPU = 'cuda:0'
+IS_HALF = True
+
+P_LEN = 224
+SKIP_HEAD = 200
+RETURN_LENGTH = 20
+RETURN_LENGTH2 = 20
+
+net_g_model: Optional[nn.Module] = None
+
+def load_net_g_model(pth_path):
+    global net_g_model
+    load_start_time = time.perf_counter()
+
+    net_g_model, cpt = get_synthesizer(pth_path=pth_path, device=GPU)
+    tgt_sr = cpt["config"][-1]
+    assert tgt_sr == 40000
+    cpt["config"][-3] = cpt["weight"]["emb_g.weight"].shape[0]
+    assert cpt.get("f0", 1) == 1
+    assert cpt.get("version", "v1") == "v2"
+    if IS_HALF:
+        net_g_model = net_g_model.half()
+    else:
+        net_g_model = net_g_model.float()
+
+    load_done_time = time.perf_counter()
+    print(f'Loaded net_g model in {(load_done_time - load_start_time) * 1000:.1f} ms')
+
+C_feats = torch.load(f'{DATA_DIR}/feats.pt').to(GPU)
+C_p_len = torch.full((10,), P_LEN, dtype=torch.long, device=GPU)
+C_cache_pitch = torch.load(f'{DATA_DIR}/cache_pitch.pt').to(GPU)
+C_cache_pitchf = torch.load(f'{DATA_DIR}/cache_pitchf.pt').to(GPU)
+C_sid = torch.zeros(10, dtype=torch.long, device=GPU)
+
+C_skip_head = torch.LongTensor([SKIP_HEAD])
+C_return_length = torch.LongTensor([RETURN_LENGTH])
+C_return_length2 = torch.LongTensor([RETURN_LENGTH2])
+
+class NetGWrapper(nn.Module):
+    def __init__(self, orig_net_g_model: nn.Module):
+        super().__init__()
+        self.model = orig_net_g_model.eval()
+
+    def forward(
+        self,
+        feats,
+        p_len,
+        pitch,
+        pitchf,
+        sid,
+        skip_head,
+        return_length,
+        return_length2,
+    ) -> torch.Tensor:
+        return self.model.infer(
+            feats,
+            p_len,
+            pitch,
+            pitchf,
+            sid,
+            skip_head,
+            return_length,
+            return_length2,
+        )[0]
+
+def export_to_onnx(onnx_filename):
+    model = NetGWrapper(orig_net_g_model=net_g_model)
+    model.eval()
+
+    torch.onnx.export(
+        model,
+        (
+            C_feats,
+            C_p_len,
+            C_cache_pitch,
+            C_cache_pitchf,
+            C_sid,
+            C_skip_head,
+            C_return_length,
+            C_return_length2,
+        ),
+        onnx_filename,
+        input_names=[
+            'feats',
+            'p_len',
+            'pitch',
+            'pitchf',
+            'sid',
+            'skip_head',
+            'return_length',
+            'return_length2',
+        ],
+        output_names=['audio'],
+        dynamic_axes={
+            'feats': {0: 'batch_size', 1: 'p_len'},
+            'p_len': {0: 'batch_size'},
+            'pitch': {0: 'batch_size', 1: 'p_len'},
+            'pitchf': {0: 'batch_size', 1: 'p_len'},
+            'sid': {0: 'batch_size'},
+        },
+        opset_version=17,
+        export_params=True,
+        do_constant_folding=True,
+        dynamo=True,
+        external_data=False,
+    )
+
+if __name__ == '__main__':
+    load_net_g_model('../assets/weights/voicevox_speaker_43.pth')
+    export_to_onnx('voicevox_speaker_43.onnx')
